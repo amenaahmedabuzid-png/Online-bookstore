e21d53f84fd25ac3a81509c4892084907c581f7f
malakabdelazimsa <malak.abdelazim@msa.edu.eg>
2025-11-12 03:37:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple button click interaction
+document.getElementById('viewMore').addEventListener('click', function() {
+  alert('Coming soon! Check back for more details on Wild Reverence 💫');
+});
